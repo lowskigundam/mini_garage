ba4c5c6ac13b14a177c8c1b567f9d1b514d926d9
lowskigundam <155948039+lowskigundam@users.noreply.github.com>
2026-04-05 14:00:53 +0700

    login with email

---
+++ lib/login_screen.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'services/auth_service.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final AuthService _auth = AuthService();
+
+  final emailController = TextEditingController();
+
+  final passwordController = TextEditingController();
+
+  bool isLogin = true;
+
+  void handleAuth() async {
+    try {
+      if (isLogin) {
+        await _auth.signIn(
+          emailController.text.trim(),
+          passwordController.text.trim(),
+        );
+      } else {
+        await _auth.signUp(
+          emailController.text.trim(),
+          passwordController.text.trim(),
+        );
+      }
+    } catch (e) {
+      print("Auth error: $e");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(isLogin ? "Login" : "Sign Up")),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(
+              controller: emailController,
+              decoration: const InputDecoration(labelText: "Email"),
+            ),
+            TextField(
+              controller: passwordController,
+              decoration: const InputDecoration(labelText: "Password"),
+              obscureText: true,
+            ),
+            const SizedBox(height: 20),
+
+            ElevatedButton(
+              onPressed: handleAuth,
+              child: Text(isLogin ? "Login" : "Sign Up"),
+            ),
+
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  isLogin = !isLogin;
+                });
+              },
+              child: Text(isLogin ? "Create account" : "Already have account"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ import 'package:provider/provider.dart';
 import 'vehicle_provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,7 +19,10 @@ void main() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(
-    ChangeNotifierProvider(create: (_) => VehicleProvider(), child: MyApp()),
+    ChangeNotifierProvider(
+      create: (_) => VehicleProvider(),
+      child: const AuthGate(),
+    ),
   );
 }
 
@@ -92,6 +97,32 @@ class _HomeScreenState extends State<HomeScreen> {
                 );
               },
             ),
+    );
+  }
+}
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Scaffold(
+              body: Center(child: CircularProgressIndicator()),
+            );
+          }
+
+          if (snapshot.hasData) {
+            return MyApp(); // logged in
+          } else {
+            return const LoginScreen(); // not logged
+          }
+        },
+      ),
     );
   }
 }
