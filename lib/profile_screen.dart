64af3e9bb689697f017fcba696ecde459cd22294
lowskigundam <155948039+lowskigundam@users.noreply.github.com>
2026-04-05 14:08:11 +0700

    logout function

--- lib/profile_screen.dart
+++ lib/profile_screen.dart
@@ -1,14 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'User Profile',
-        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    final user = FirebaseAuth.instance.currentUser;
+
+    return Scaffold(
+      appBar: AppBar(title: const Text("Profile")),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("Logged in as:", style: TextStyle(fontSize: 16)),
+
+            const SizedBox(height: 10),
+
+            Text(
+              user?.email ?? "No email",
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+
+            const SizedBox(height: 30),
+
+            ElevatedButton(
+              onPressed: () async {
+                await FirebaseAuth.instance.signOut();
+              },
+              child: const Text("Logout"),
+            ),
+          ],
+        ),
       ),
     );
   }
