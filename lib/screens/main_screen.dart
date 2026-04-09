6d1100aa30d686b04fc4a18f78f1d53a30baa1ca
lowskigundam <155948039+lowskigundam@users.noreply.github.com>
2026-04-09 13:38:04 +0700

    update add vehicle button

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -28,72 +28,67 @@ class _MainScreenState extends State<MainScreen> {
         ],
       ),
 
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          final result = await Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => AddVehicleScreen()),
-          );
-
-          if (result != null) {
-            final provider = Provider.of<VehicleProvider>(
-              context,
-              listen: false,
-            );
-
-            provider.addVehicle(result);
-          }
-        },
-        child: Icon(Icons.directions_car),
-        backgroundColor: Colors.black,
-      ),
-
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-
-      bottomNavigationBar: BottomAppBar(
-        shape: CircularNotchedRectangle(),
-        notchMargin: 8,
-        child: SizedBox(
-          height: 60,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              // HOME
-              IconButton(
-                icon: Icon(
-                  Icons.home,
-                  color: currentIndex == 0 ? Colors.black : Colors.grey,
-                ),
-                onPressed: () {
-                  setState(() => currentIndex = 0);
-                },
+      bottomNavigationBar: Container(
+        height: 70,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [BoxShadow(color: Colors.black12, blurRadius: 10)],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            // HOME
+            IconButton(
+              icon: Icon(
+                Icons.home,
+                color: currentIndex == 0 ? Colors.black : Colors.grey,
               ),
+              onPressed: () {
+                setState(() => currentIndex = 0);
+              },
+            ),
 
-              // STATS
-              IconButton(
-                icon: Icon(
-                  Icons.bar_chart,
-                  color: currentIndex == 1 ? Colors.black : Colors.grey,
-                ),
-                onPressed: () {
-                  setState(() => currentIndex = 1);
-                },
+            // STATS
+            IconButton(
+              icon: Icon(
+                Icons.bar_chart,
+                color: currentIndex == 1 ? Colors.black : Colors.grey,
               ),
+              onPressed: () {
+                setState(() => currentIndex = 1);
+              },
+            ),
 
-              const SizedBox(width: 40),
+            // ADD VEHICLE (NOW NORMAL TAB)
+            IconButton(
+              icon: Icon(Icons.directions_car, color: Colors.black),
+              onPressed: () async {
+                final result = await Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => AddVehicleScreen()),
+                );
 
-              // PROFILE
-              IconButton(
-                icon: Icon(
-                  Icons.person,
-                  color: currentIndex == 3 ? Colors.black : Colors.grey,
-                ),
-                onPressed: () {
-                  setState(() => currentIndex = 3);
-                },
+                if (result != null) {
+                  final provider = Provider.of<VehicleProvider>(
+                    context,
+                    listen: false,
+                  );
+                  provider.addVehicle(result);
+                }
+              },
+            ),
+
+            // PROFILE
+            IconButton(
+              icon: Icon(
+                Icons.person,
+                color: currentIndex == 3 ? Colors.black : Colors.grey,
               ),
-            ],
-          ),
+              onPressed: () {
+                setState(() => currentIndex = 3);
+              },
+            ),
+          ],
         ),
       ),
     );
